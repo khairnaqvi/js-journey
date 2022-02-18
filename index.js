86a71ca7d7a249064ef4dec4bc4742d5e39f66c4
Muhammad Umair <umair@blackandblackcreative.com>
2022-02-18 19:58:09 +0500

    custom merge with origin

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 JS.DATA = [
     {
-    id:1,
-    name:'Ali',
-    age:'23',
-    city:'lahore',
-},
-{
         id:1,
-        name:'khair',
-        age:'40',
-        city:'pindi',
-},
-{
+        name:'Ali',
+        age:'23',
+        city:'lahore',
+    },
+    {
         id:1,
         name:'Umair',
         age:'24',
         city:'Islamabad',
-    },{
-id:1,
-name:'khair',
-age:'40',
-city:'pindi',
-}
-
+    },
+    {
+        id:1,
+        name:'khair',
+        age:'40',
+        city:'pindi',
+    }
 ]
     
